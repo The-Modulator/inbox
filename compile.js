a95d37bd5d076c64161935b3bf72168e8a9ccd18
Elias Gabriel <elias_ghabriel@hotmail.com>
2023-08-23 15:32:50 +0100

    deploy sepolia

--- compile.js
+++ compile.js
@@ -1,8 +1,12 @@
 const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
+const { log } = require('console');
 
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-module.exports = solc.compile(source, 1).contracts[':Inbox'];
+// module.exports = solc.compile(source, 1).contracts[':Inbox'];
+
+console.log(solc.compile(source, 1).contracts[':Inbox']);
+
--- deploy.js
+++ deploy.js
@@ -1,5 +1,28 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const { Web3 } = require('web3');
+const {interface, bytecode} = require('./compile');
 //updated web3 and hdwallet-provider imports added for convenience
 
 // deploy code will go here
+const provider = new HDWalletProvider(
+    'soldier question general speak satisfy enough ticket office song blind today banana',
+    'https://sepolia.infura.io/v3/f1f3e03349d54277ba8a37d2ca89106f'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    
+    console.log('Attempting to deploy from account', accounts[0])
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments:['Hi there!'] })
+        .send({ gas:'1000000', from: accounts[0]});
+
+    console.log('Contract deployed to', result.options.address);
+
+    provider.engine.stop();
+};
+
+deploy();
